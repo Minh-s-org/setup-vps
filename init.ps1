b16d63cb5cc19bd58a883900eda9f55ca92869b1
minh.hc <minh.hc@geekup.vn>
2024-03-26 15:06:44 +0700

    update

---
+++ init.ps1
@@ -0,0 +1,8 @@
+### chocolatey
+Write-Host "Installing chocolatey..."
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+Write-Host "Installed chocolatey"
+
+Write-Host "Installing Git..."
+choco install -y git
+Write-Host "Installed Git..."
