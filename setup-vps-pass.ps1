e73c92e5be6008787b9f7f9caf86ddad4b0ad6de
minh.hc <minh.hc@geekup.vn>
2024-04-02 09:40:30 +0700

    add

--- setup-vps-pass.ps1
+++ setup-vps-pass.ps1
@@ -1,3 +1,3 @@
 Write-Host "Enter new password for VPS"
 $password=Read-Host -AsSecureString
-Set-LocalUser -Name administrator -Password $password
+Set-LocalUser -Name $Env:UserName -Password $password
