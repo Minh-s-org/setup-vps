ec1b3ba445b6f4bfe363f7f6cabad10e0af5b98e
minh.hc <minh.hc@geekup.vn>
2024-04-03 23:24:12 +0700

    update

--- utils/init-node.ps1
+++ utils/init-node.ps1
@@ -3,7 +3,7 @@ $BASE_PATH="$env:USERPROFILE"
 Set-Location $BASE_PATH
 
 Write-Host "Removing old node on VPS"
-Remove-Item -Path .\.mysterium-node,.\.myst_node_launcher,.\.mysterium-bin -Force -Recurse -ErrorAction SilentlyContinue
+Remove-Item -Path $BASE_PATH\.mysterium-node,$BASE_PATH\.myst_node_launcher,$BASE_PATH\.mysterium-bin -Force -Recurse -ErrorAction SilentlyContinue
 
 Write-Host "Starting Myst Launcher on VPS"
 Start-Process -FilePath "C:\Program Files\Mysterium Launcher\myst-launcher-amd64.exe"
