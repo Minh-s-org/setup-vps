b365550bda9e74ec391787327f20d5278faa5fc4
minh.hc <minh.hc@geekup.vn>
2024-04-05 08:52:40 +0700

    update

--- renew-node.ps1
+++ renew-node.ps1
@@ -10,16 +10,16 @@ param (
 $BASE_PATH="C:\\Users\\$user"
 
 $secpasswd = ConvertTo-SecureString $pass -AsPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential ($username, $secpasswd)
+$credential = New-Object System.Management.Automation.PSCredential ($user, $secpasswd)
 
 Write-Host "Packaging node on VPS"
-$session = New-SSHSession -ComputerName $server -Credential $credential -AcceptKey
+$session = New-SSHSession -ComputerName $vpsIp -Credential $credential -AcceptKey
 $sessionId = $session.SessionId 
 Invoke-SSHCommand -Command "powershell -ExecutionPolicy Bypass -File $BASE_PATH\\setup-vps\\utils\\package-node.ps1" -SessionId $sessionId
 Remove-SSHSession -Session $sessionId
 
 Write-Host "Copying VPS node to local"
-Get-SCPItem -ComputerName $server -Credential $credential -Path "C:\Users\$user\node.zip" -PathType File -Destination ./
+Get-SCPItem -ComputerName $vpsIp -Credential $credential -Path "C:\Users\$user\node.zip" -PathType File -Destination ./
 
 Write-Host "Stoping Myst Launcher"
 Stop-Process -Name *myst* -Force
@@ -34,7 +34,7 @@ Write-Host "Starting Myst Launcher"
 Start-Process -FilePath "C:\Program Files\Mysterium Launcher\myst-launcher-amd64.exe"
 
 Write-Host "Init new node on VPS"
-$session = New-SSHSession -ComputerName $server -Credential $credential -AcceptKey
+$session = New-SSHSession -ComputerName $vpsIp -Credential $credential -AcceptKey
 $sessionId = $session.SessionId 
 Invoke-SSHCommand -Command "powershell -ExecutionPolicy Bypass -File $BASE_PATH\\setup-vps\\utils\\init-node.ps1" -SessionId $sessionId
 Remove-SSHSession -Session $sessionId
